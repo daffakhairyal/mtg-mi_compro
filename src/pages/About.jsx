b9fd0fa2fbe2aef45ffbc54c11268c84844d24ec
unknown <daffakhairyal@gmail.com>
2024-10-06 11:56:11 +0700

    update overview

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,61 @@
-import React from 'react'
-import { useInView } from 'react-intersection-observer';
-import Overview from '../components/Overview'
-import VisionMission from '../components/VisionMission'
-import "./About.css"
+import React, { useEffect, useRef, useState } from 'react';
+import Overview from '../components/Overview';
+import VisionMission from '../components/VisionMission';
+import "./About.css";
 
 const About = () => {
-  const { ref: overviewRef, inView: overviewInView } = useInView({ threshold: 0.1 });
-  const { ref: visionMissionRef, inView: visionMissionInView } = useInView({ threshold: 0.1 });
+  const [isOverviewVisible, setIsOverviewVisible] = useState(false);
+  const [isVisionMissionVisible, setIsVisionMissionVisible] = useState(false);
+
+  const overviewRef = useRef(null);
+  const visionMissionRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null, 
+      rootMargin: "0px",
+      threshold: 0.1, 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (entry.target === overviewRef.current) {
+            setIsOverviewVisible(true);
+          }
+          if (entry.target === visionMissionRef.current) {
+            setIsVisionMissionVisible(true);
+          }
+        }
+      });
+    }, options);
+
+    if (overviewRef.current) observer.observe(overviewRef.current);
+    if (visionMissionRef.current) observer.observe(visionMissionRef.current);
+
+    // Clean up observer
+    return () => {
+      if (overviewRef.current) observer.unobserve(overviewRef.current);
+      if (visionMissionRef.current) observer.unobserve(visionMissionRef.current);
+    };
+  }, []);
 
   return (
     <div className='text-white max-w-7xl mx-auto pt-20 px-6'>
       <div className='md:pt-10'>
         <div 
           ref={overviewRef} 
-          className={`animation-scroll ${overviewInView ? 'animate' : ''}`}>
+          className={`animation-scroll ${isOverviewVisible ? 'animate' : ''}`}>
           <Overview />
         </div>
         <div 
           ref={visionMissionRef} 
-          className={`animation-scroll ${visionMissionInView ? 'animate' : ''}`}>
+          className={`animation-scroll ${isVisionMissionVisible ? 'animate' : ''}`}>
           <VisionMission />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About;
