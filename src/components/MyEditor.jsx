6743111282d33f16c909ac8830945ec3633b5156
unknown <daffakhairyal@gmail.com>
2024-11-08 23:39:34 +0700

    update

---
+++ src/components/MyEditor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyEditor = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyEditor
--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import MyEditor from '../components/MyEditor'
 
 const Job = () => {
   return (
     <div className='text-white max-w-7xl mx-auto pt-20 px-6'>
      <div className='md:pt-20'>
-     Job Opportunity
+     <MyEditor/>
      </div>
     </div>
   )
